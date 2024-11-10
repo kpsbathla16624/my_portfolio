1ec73b2c7944265306ebed120e197ad1e584aafa
kamal <kamalpreet6198@gmail.com>
2024-11-10 21:28:13 +0530

    Replace iframe with object tag for resume PDF display

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -5,12 +5,10 @@ function ResumePage() {
   return (
     <div className="relative z-10 w-full flex flex-col justify-center items-center">
       <h1 className="m-10 text-4xl">My Resume</h1>
-      <iframe 
-        src="/resume/resumekps.pdf" 
-        className="w-5/6 h-[100vh]" 
-        title="Resume"
-        
-      ></iframe>
+      <object data="/resume/resumekps.pdf" type="application/pdf" className="w-5/6 h-[100vh]" title="Resume">
+  <p>Your browser does not support PDFs. <a href="/resume/resumekps.pdf">Download the PDF</a>.</p>
+</object>
+
     </div>
   );
 }
