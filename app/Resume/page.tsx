5c9c06faaafce4b5f839b1aed055934a0926044b
kamal <kamalpreet6198@gmail.com>
2024-11-10 21:51:15 +0530

    update Content Security Policy in next.config.ts

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,14 +1,16 @@
-
+"use client";
 import React from 'react';
 
 function ResumePage() {
   return (
     <div className="relative z-10 w-full flex flex-col justify-center items-center">
       <h1 className="m-10 text-4xl">My Resume</h1>
-      <object data="/resume/resumekps.pdf" type="application/pdf" className="w-5/6 h-[100vh]" title="Resume">
-  <p>Your browser does not support PDFs. <a href="/resume/resumekps.pdf">Download the PDF</a>.</p>
-</object>
-
+      <iframe 
+        src="/resume/resumekps.pdf" 
+        className="w-5/6 h-[100vh]" 
+        title="Resume"
+        
+      ></iframe>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Custom configuration options
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-src 'self'; object-src 'none';"
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
