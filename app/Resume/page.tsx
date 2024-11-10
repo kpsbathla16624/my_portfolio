800ccf73690f1500fceeb694ecfe15703243844f
kamal <kamalpreet6198@gmail.com>
2024-11-10 21:23:40 +0530

    added resume pdf

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+
+import React from 'react';
 
 function ResumePage() {
   return (
-    <div>
-      
+    <div className="relative z-10 w-full flex flex-col justify-center items-center">
+      <h1 className="m-10 text-4xl">My Resume</h1>
+      <iframe 
+        src="/resume/resumekps.pdf" 
+        className="w-5/6 h-[100vh]" 
+        title="Resume"
+        
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default ResumePage
+export default ResumePage;
