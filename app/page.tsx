626e5d8b2e0986ec03c42205f9c3d1cd9e1c165d
kamal <kamalpreet6198@gmail.com>
2024-11-10 20:37:54 +0530

    css fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Photo from './components/photo'
 
 function HomePage() {
   return (
-    <div className='sm:flex-row flex flex-col relative z-10 mx-5 space-y-5 justify-center   items-center sm:justify-between lg:mx-2 xl:mx-10 my-10' >
+    <div className='sm:flex-row flex flex-col relative z-10 mx-5 space-y-5 justify-center   items-center sm:justify-between lg:mx-10 xl:mx-20 my-10' >
       <Photo/>
       <div className=' space-y-5 '>
         <h1 className='text-3xl' >Hi!, I am </h1>
         
-        <p className='font-serif font-bold text-3xl lg:text-6xl'>Kamal Preet Singh</p>
+        <p className='font-serif font-bold text-3xl md:text-4xl lg:text-6xl'>Kamal Preet Singh</p>
          </div>
     </div>
   )
