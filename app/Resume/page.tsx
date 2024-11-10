42e109b45984f0239e46e217271c77e4aaa171f6
kamal <kamalpreet6198@gmail.com>
2024-11-10 22:10:02 +0530

    update resume PDF path and remove outdated file

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -6,7 +6,7 @@ function ResumePage() {
     <div className="relative z-10 w-full flex flex-col justify-center items-center">
       <h1 className="m-10 text-4xl">My Resume</h1>
       <iframe 
-        src="/resume/resumekps.pdf" 
+        src="/resumekps.pdf" 
         className="w-5/6 h-[100vh]" 
         title="Resume"
         
