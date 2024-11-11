af2b8e105e2c45cd54c4f1749fcf02f49d50771a
kamal <kamalpreet6198@gmail.com>
2024-11-11 09:56:22 +0530

    comment out iframe for resume display

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -5,12 +5,12 @@ function ResumePage() {
   return (
     <div className="relative z-10 w-full flex flex-col justify-center items-center">
       <h1 className="m-10 text-4xl">My Resume</h1>
-      <iframe 
+      {/* <iframe 
         src="/resumekps.pdf" 
         className="w-5/6 h-[100vh]" 
         title="Resume"
         
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
