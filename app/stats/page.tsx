191f405d1858dfc1ef854a074bdfca5ae6501154
kamal <kamalpreet6198@gmail.com>
2024-11-30 13:17:18 +0530

    github stats

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import QuestionCountGraph from './QuestionBar'
+import QuestionCountGraph from './QuestionBar';
+
+import GitHubCalendar from "react-github-calendar";
 
 
 
 
 function StatsPage() {
   return (
-    <div id='stats' className='z-50 relative  text-white'  >
-      stats to be added
+    <div id='stats' className='z-50 px-10 relative  text-white'  >
+     
       <QuestionCountGraph/>
+     
+         <div className='border border-white space-y-2 flex flex-col w-min p-2 rounded-3xl'>
+         <h1 className='text-2xl'>Github Contibutions</h1>
+
+      <GitHubCalendar username="kpsbathla16624"  />
+         </div>
+   
     
     </div>
   )
