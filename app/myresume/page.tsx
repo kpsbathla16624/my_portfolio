3c401b71aba242f95935a2a35f88d2b42c3277f0
kamal <kamalpreet6198@gmail.com>
2024-12-03 03:32:42 +0530

    RESUME UPDATE

--- app/myresume/page.tsx
+++ app/myresume/page.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 function ResumePage() {
   return (
     <div className="relative z-10 w-full flex flex-col justify-center items-center">
-      <h1 className="m-10 text-4xl">My Resume</h1>
+      <h1 className="py-5 sm:py-10 text-2xl sm:text-3xl lg:text-5xl text-primary font-share-tech-mono text-center md:text-left">
+          My Resume
+        </h1>
       <iframe 
-        src="/resumekps.pdf" 
+        src="/Kamal_preet_resume.pdf" 
         className="w-5/6 h-[100vh]" 
         title="Resume"
         
